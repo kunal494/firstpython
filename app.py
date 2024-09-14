0c34f0ba7256bfbd4003a7897fa6396c8ae1bae2
kunal494 <samantokunal494@gmail.com>
2024-09-15 00:10:20 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+# guess = 25
+# attempts = 5
+# correct = False
+
+# for _ in range(attempts):
+#     user = int(input("Guess the number: "))
+#     if user == guess:
+#         print("Congratulations, you've guessed correctly!")
+#         correct = True
+#         break
+#     else:
+#         print("Sorry, that's not the correct number. Try again!")
+
+# if not correct:
+#     print("You've used all your attempts. Better luck next time!")
+
+
---
+++ python lecturre 1.py
@@ -0,0 +1,59 @@
+# name="kunal"
+# age=19
+# price=80.77
+# print("my name is:",name)
+# print("my age is:",age)
+
+
+# a=5
+# b=5
+# sum=a*b
+# print(sum)
+
+
+
+# a,b=2,5
+# txt="@"
+# print(txt*a*b)
+
+
+
+# a,b="3",4
+# txt="@"
+# print((a+txt)*b)
+
+
+#input string value in python
+# name= input("name :")
+# print(name)
+#input int value in python
+
+
+# age = int(input("Age: "))
+# print(type(age))
+# print(age)
+
+
+# name=input("Name :")
+# age=int(input("Age :"))
+# price=float(input("Price :"))
+# print("My name is",name,"My age is",age)
+
+
+
+
+#conditonal statement if,elif,else
+light=input("Enter color :").lower()
+print(light)
+
+
+if (light == "red"):
+    print("stop")
+elif( light == "yeliow"):
+    print("Look")
+elif(light == "green"):
+    print("Go")
+else:
+    print("Invalid color")
+
+    
